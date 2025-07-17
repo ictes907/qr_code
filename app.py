628b18d6e6b1d0fd468ccecaac92f5e8ebac4635
Icte <ictes907@example.com>
2025-07-17 08:51:46 +0300

    ربط التطبيق بمنفذ Render الديناميكي

--- app.py
+++ app.py
@@ -1024,8 +1024,9 @@ def scan_qr():
 
 # ✅ تشغيل التطبيق
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0')
 
-    app.run(host='0.0.0.0', port=10000)
+   port = int(os.environ.get("PORT", 10000))
+   app.run(host="0.0.0.0", port=port)
+
 
 
