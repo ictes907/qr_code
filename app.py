bc80a1a9f0ca632c5b93b5507b678713cd9011d6
Icte <ictes907@example.com>
2025-07-17 09:04:51 +0300

    ربط Flask بمنفذ 0.0.0.0 عبر PORT من Render

--- app.py
+++ app.py
@@ -1026,8 +1026,9 @@ def scan_qr():
 import os
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 10000))
-    app.run(host="0.0.0.0", port=port)
+    port = int(os.environ.get("PORT", 10000))  # رقم المنفذ يجي من Render
+    app.run(host='0.0.0.0', port=port)         # لازم يكون 0.0.0.0 وليس localhost
+
 
 
 
