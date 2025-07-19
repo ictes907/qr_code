bc33cb13da451af246bbf25901094eba67013ef5
Icte <ictes907@example.com>
2025-07-19 07:48:35 +0300

    ضبط البورت لـ Render

--- app.py
+++ app.py
@@ -1081,6 +1081,8 @@ def scan_qr():
 if __name__ == '__main__':
     import os
     port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
+
     app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
 
 
