fd1e99db1ddd602815ac37f4035ed070bddc1aa3
Icte <ictes907@example.com>
2025-07-22 10:57:36 +0300

    تصحيح كلمة المرور النهائية من Neon

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ def get_db_connection():
     return psycopg2.connect(
         dbname="neondb",
         user="neondb_owner",
-        password="npg_BlwozyJYOTPK",
+        password="npg_WHVUR3xdZ6OY",
         host="ep-withered-snow-aeck2ex1-pooler.c-2.us-east-2.aws.neon.tech",
         port="5432",
         sslmode="require"
