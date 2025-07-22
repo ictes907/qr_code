1f76865472fe664bc1c13e6746da268a2b3817c9
Icte <ictes907@example.com>
2025-07-22 09:42:00 +0300

    تحديث كلمة المرور لاتصال Neon

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ def get_db_connection():
     return psycopg2.connect(
         dbname="neondb",
         user="neondb_owner",
-        password="npg_VU8tyFNlW0IK",
+        password="npg_JhZQv0DABr2t",
         host="ep-withered-snow-aeck2ex1-pooler.c-2.us-east-2.aws.neon.tech",
         port="5432",
         sslmode="require"
