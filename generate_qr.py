2163deeb91b0e0d54f8c553ddcf34946f6501cee
Icte <ictes907@example.com>
2025-07-23 08:26:34 +0300

    تعديل كود الحضور وإصلاح رابط QR

--- generate_qr.py
+++ generate_qr.py
@@ -1,6 +1,7 @@
 import qrcode
 
-url = "https://qr-attendance-app-tgfx.onrender.com/confirm_attendance?course_id=4&student_id=S1001"
+url = "https://qr-attendance-app-tgfx.onrender.com/confirm_attendance?course_id=2&student_id=S1001"
 img = qrcode.make(url)
-img.save("qr_course_4.png")
+img.save("static/qr_course_2.png")
+
 
