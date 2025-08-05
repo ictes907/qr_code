d73b0afaf725f97164ffc49bcc95cb0b6da529f3
Icte <ictes907@example.com>
2025-08-05 08:19:25 +0300

    🔧 إضافة راوت sync_courses + ضبط الاتصال بـ mambe و neon

--- mamp_conn.py
+++ mamp_conn.py
@@ -4,7 +4,7 @@ def get_mamp_connection():
     return psycopg2.connect(
         host="localhost",       # أو عنوان السيرفر
         database="attendance_system",    # اسم قاعدة Mambe
-        user="host",        # اسم المستخدم
-        password="",
+        user="root",        # اسم المستخدم
+        password="root",
         port=5432               # المنفذ الافتراضي
     )
