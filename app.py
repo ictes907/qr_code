ebb24727411f4a46bf64b028e8bfe9616869749c
Icte <ictes907@example.com>
2025-08-10 04:28:23 +0300

    تعديل الاتصال بـ MySQL وتحديث mamp_conn.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for, session, jsonify, send_file
 import os
 import psycopg2
+import pymysql
 import qrcode
 import pandas as pd
 from io import BytesIO
--- mamp_conn.py
+++ mamp_conn.py
@@ -1,10 +1,10 @@
-import psycopg2
-
 def get_mamp_connection():
-    return psycopg2.connect(
-        host="localhost",
-        database="attendance_system",
+    import pymysql
+    return pymysql.connect(
+        host="127.0.0.1",
+        port=3306,
         user="root",
         password="root",
-        port=8889  # حسب إعدادات MAMP
+        database="attendance_system"
     )
+
